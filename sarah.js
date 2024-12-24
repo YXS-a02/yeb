162b4b058b577a244f90eaa5712d06df9d374610
yxs <atwyxsa02@163.com>
2024-12-24 18:27:10 +0800

    3.5

---
+++ sarah.js
@@ -0,0 +1,11 @@
+function sarah(){
+    var sarah=document.getElementById("sarah").value
+    
+    if (sarah=="yeb") {
+       alert("Is there!") 
+    }
+    else
+    {
+        alert("sorry，I don't know!")
+    }
+}
